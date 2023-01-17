e70b7a284124bab0e146b2a9edc106c1851f3aae
Maxwellnt <pau.martinez@gracia.lasalle.cat>
2023-01-17 09:53:17 +0100

    Create MartinezPau.java

---
+++ Exercicis/src/MartinezPau.java
@@ -0,0 +1,20 @@
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import  java.net.InetAddress;
+public class MartinezPau {
+    public static void main(String[] args) throws UnknownHostException {
+        Benvinguda();
+    }
+
+    public static void Benvinguda() throws UnknownHostException {
+
+        InetAddress addr = InetAddress.getLocalHost();
+        Scanner llegir = new Scanner(System.in);
+        String name = addr.getHostName();
+        System.out.println("Benvulgut, "+ name);
+    }
+
+
+
+
+}
